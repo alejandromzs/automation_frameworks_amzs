c0e75119e61e279fa476d4a736ab8f77fcba097c
Alejandro <almazesi@gmail.com>
2024-09-23 22:21:03 -0500

    cypress 2_2 fixed

--- cypressJSNodeMac/2_IntermediateScript/cypress/e2e/2_2_test.cy.js
+++ cypressJSNodeMac/2_IntermediateScript/cypress/e2e/2_2_test.cy.js
@@ -6,30 +6,48 @@ describe('testApp' , () =>{
         cy.visit('https://parabank.parasoft.com/parabank/index.htm');
     })
 
-    it ('frontend can be opened -minimal check' , ()=>{
+    it ('Test: frontend can be opened - minimal check' , ()=>{
        
         cy.contains('www.parasoft.com')
         
     }
     )
 
-    it ('Login form pass' , ()=>{
+    it ('Register an account' , ()=>{
+        cy.visit('https://parabank.parasoft.com/parabank/index.htm');
+        cy.xpath('//a[.="Register"]').click();
+        cy.get('#customer\\.firstName').type('testFirstName');
+        cy.get('#customer\\.lastName').type('testLastName');
+        cy.get('#customer\\.address\\.street').type('custAddStreet');
+        cy.get('#customer\\.address\\.city').type('custAddcity');
+        cy.get('#customer\\.address\\.state').type('custAddState');
+        cy.get('#customer\\.address\\.zipCode').type('custAddZipCode');
+        cy.get('#customer\\.phoneNumber').type('custPhoneNumber');
+        cy.get('#customer\\.ssn').type('custSsn');
+        cy.get('#customer\\.username').type('test');
+        cy.get('#customer\\.password').type('test');
+        cy.get('#repeatedPassword').type('test');
+        cy.get('input[value="Register"]').click();
+    }
+    )
+
+    it ('Test: Login form pass' , ()=>{
        
         cy.contains('Username')
         cy.xpath('//input[@name="username"]').type('test')
         cy.xpath('//input[@name="password"]').type('test')
         cy.get('input.button').click();
         cy.contains('Log Out')
-        cy.get('#leftPanel').find('a href="openaccount.htm"')
+        cy.contains('Welcome')
         
     }
     )
 
-    it ('Login fail' , ()=>{
+    it ('Test: Login fail' , ()=>{
         cy.visit('https://parabank.parasoft.com/parabank/index.htm');
         cy.contains('Username')
         cy.xpath('//input[@name="username"]').type('test1')
-        cy.xpath('//input[@name="password"]').type('test2')
+        cy.xpath('//input[@name="password"]').type('test22312803')
         cy.get('input.button').click()
         cy.get('input[value="Log In"]')
         
